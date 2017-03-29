cd33767e1a7e4f8f3670e06659075bb8f8938521
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2017-03-30 01:37:20 +0300

    Update ConnectOffice365.ps1

--- ConnectOffice365.ps1
+++ ConnectOffice365.ps1
@@ -4,6 +4,16 @@ $UserCredential = Get-Credential
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection 
 Import-PSSession $Session 
 
-$msol servisine bağlanmak için 
+#msol servisine bağlanmak için 
 $UserCredential = Get-Credential 
 Connect-MsolService -Credential $UserCredential 
+
+#Sharepoint Servisine Bağlanmak için 
+Import-Module Microsoft.Online.SharePoint.PowerShell -DisableNameChecking 
+Connect-SPOService -Url https://domain-admin.sharepoint.com -credential $credential 
+ 
+#Skype for Business Servisine Bağlanmak için  
+Import-Module SkypeOnlineConnector 
+$SkypeSession = New-CsOnlineSession -Credential $credential 
+Import-PSSession $SkypeSession 
+ 
